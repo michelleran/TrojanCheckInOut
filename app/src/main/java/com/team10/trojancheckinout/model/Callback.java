a76e8094dfef600fba56ec1ac37f31aba676565e
michelleran <ranmiche@usc.edu>
2021-03-03 10:06:18 -0500

    Add doc for Callback, Listener

--- app/src/main/java/com/team10/trojancheckinout/model/Callback.java
+++ app/src/main/java/com/team10/trojancheckinout/model/Callback.java
@@ -1,5 +1,21 @@
 package com.team10.trojancheckinout.model;
 
+/**
+ * <p>How to pass to a method:</p>
+ * <pre>{@code
+ * myMethod(..., new Callback<T>() {
+ *      @Override
+ *      public void onSuccess(T result) {
+ *          // ...
+ *      }
+ *
+ *      @Override
+ *      public void onFailure(Exception exception) {
+ *          // ...
+ *      }
+ * });
+ * }</pre>
+ */
 public interface Callback<T> {
     void onSuccess(T result);
     void onFailure(Exception exception);
--- app/src/main/java/com/team10/trojancheckinout/model/Listener.java
+++ app/src/main/java/com/team10/trojancheckinout/model/Listener.java
@@ -1,5 +1,23 @@
 package com.team10.trojancheckinout.model;
 
+/**
+ * <p>How to pass to a method:</p>
+ * <pre>{@code
+ * myMethod(..., new Listener<T>() {
+ *      @Override
+ *      public void onAdd(T item) {
+ *          // ...
+ *      }
+ *
+ *      @Override
+ *      public void onRemove(T item) {
+ *          // ...
+ *      }
+ *
+ *      // etc.
+ * });
+ * }</pre>
+ */
 public interface Listener<T> {
     void onAdd(T item);
     void onRemove(T item);
