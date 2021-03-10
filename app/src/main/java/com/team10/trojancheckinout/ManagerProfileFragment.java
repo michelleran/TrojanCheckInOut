bc4f7b1a9c039e84f6b014b373f026a1318eb038
Rithwik Sivakumar <rithwiks@usc.edu>
2021-03-10 18:54:47 -0500

    Manager Profile Edit Sample

--- app/src/main/java/com/team10/trojancheckinout/ManagerProfileFragment.java
+++ app/src/main/java/com/team10/trojancheckinout/ManagerProfileFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -12,6 +14,16 @@ import com.team10.trojancheckinout.model.Manager;
 import com.team10.trojancheckinout.model.Server;
 
 public class ManagerProfileFragment extends Fragment {
+
+    private TextView txtGivenName;
+    private TextView txtSurname;
+    private TextView txtEmail;
+    private Manager currentManager;
+    private Button btnEdit;
+    private EditText edtGivenName;
+    private EditText edtSurname;
+    private EditText edtEmail;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -20,17 +32,43 @@ public class ManagerProfileFragment extends Fragment {
 
         // TODO
 
-        TextView txtGivenName = (TextView) rootView.findViewById(R.id.txtGivenName);
-        TextView txtSurname = (TextView) rootView.findViewById(R.id.txtSurname);
-        TextView txtEmail = (TextView) rootView.findViewById(R.id.txtEmail);
+        txtGivenName = (TextView) rootView.findViewById(R.id.txtGivenName);
+        txtSurname = (TextView) rootView.findViewById(R.id.txtSurname);
+        txtEmail = (TextView) rootView.findViewById(R.id.txtEmail);
+        btnEdit = (Button) rootView.findViewById(R.id.btnEdit);
+        edtGivenName = (EditText) rootView.findViewById(R.id.edtGivenName);
+        edtSurname = (EditText) rootView.findViewById(R.id.edtSurname);
+        edtEmail = (EditText) rootView.findViewById(R.id.edtEmail);
 
-        Manager manager = (Manager) Server.getCurrentUser();
+        currentManager = (Manager) Server.getCurrentUser();
 
-        txtGivenName.setText("First Name: " + manager.getGivenName());
-        txtSurname.setText("Surname: " + manager.getSurname());
-        txtEmail.setText("Email: " + manager.getEmail());
+        txtGivenName.setText("First Name: " + currentManager.getGivenName());
+        txtSurname.setText("Surname: " + currentManager.getSurname());
+        txtEmail.setText("Email: " + currentManager.getEmail());
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onEdit();
+            }
+        });
 
         return rootView;
     }
+
+    public void onEdit() {
+        /* Toggling the visibility of the First Name, Last Name, and Email fields.
+           Making the TextView invisible and EditView visible for the edit component.
+         */
+        txtGivenName.setVisibility(View.INVISIBLE);
+        edtGivenName.setVisibility(View.VISIBLE);
+
+        txtSurname.setVisibility(View.INVISIBLE);
+        edtSurname.setVisibility(View.VISIBLE);
+
+        txtEmail.setVisibility(View.INVISIBLE);
+        edtEmail.setVisibility(View.VISIBLE);
+    }
+
 
 }
