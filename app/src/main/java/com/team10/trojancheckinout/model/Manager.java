9f8f2d5a63d033460655afbbd0d5f45d74249829
Rithwik Sivakumar <rithwiks@usc.edu>
2021-03-09 20:13:59 -0500

    Manager model updated

--- app/src/main/java/com/team10/trojancheckinout/model/Manager.java
+++ app/src/main/java/com/team10/trojancheckinout/model/Manager.java
@@ -1,4 +1,44 @@
 package com.team10.trojancheckinout.model;
 
-public class Manager {
+public class Manager implements User {
+    private String id;
+    private String givenName;
+    private String surname;
+    private String email;
+    private String photoUrl;
+
+    public Manager() {}
+
+    public Manager(String id, String givenName, String surname, String email, String photoUrl) {
+        this.id = id;
+        this.givenName = givenName;
+        this.surname = surname;
+        this.email = email;
+        this.photoUrl = photoUrl;
+    }
+
+    @Override
+    public String getIdString() {
+        return id;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
 }
