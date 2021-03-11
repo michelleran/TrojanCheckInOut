9ec01abbb4df8f36154328b6d3313fb7f29853b6
michelleran <ranmiche@usc.edu>
2021-03-11 11:04:59 -0500

    Remove hardcoded colors, remove unneeded fab

--- app/src/main/java/com/team10/trojancheckinout/ManagerActivity.java
+++ app/src/main/java/com/team10/trojancheckinout/ManagerActivity.java
@@ -29,15 +29,6 @@ public class ManagerActivity extends AppCompatActivity {
         viewPager.setAdapter(managerPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-            }
-        });
     }
 }
 
