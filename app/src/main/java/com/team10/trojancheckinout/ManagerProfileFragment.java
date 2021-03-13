b556b5fe13e1d1c01a2b0e7ac965b7c023b87a33
Rithwik Sivakumar <rithwiks@usc.edu>
2021-03-13 01:15:23 -0500

    upload image logic

--- app/src/main/java/com/team10/trojancheckinout/ManagerProfileFragment.java
+++ app/src/main/java/com/team10/trojancheckinout/ManagerProfileFragment.java
@@ -1,10 +1,8 @@
 package com.team10.trojancheckinout;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +18,8 @@ import com.bumptech.glide.Glide;
 import com.team10.trojancheckinout.model.Manager;
 import com.team10.trojancheckinout.model.Server;
 
+import static android.app.Activity.RESULT_OK;
+
 public class ManagerProfileFragment extends Fragment {
 
     private TextView txtGivenName;
@@ -27,11 +27,13 @@ public class ManagerProfileFragment extends Fragment {
     private TextView txtEmail;
     private Manager currentManager;
     private Button btnEdit;
+    private Button btnChangePicture;
     private Button btnLogout;
     private EditText edtNewPassword;
     private EditText edtConfirmPassword;
     private ImageView imgPhoto;
     private int viewState;
+    private static final int SELECT_PHOTO_REQUEST = 1;
 
 
 
@@ -47,6 +49,7 @@ public class ManagerProfileFragment extends Fragment {
         txtSurname = (TextView) rootView.findViewById(R.id.txtSurname);
         txtEmail = (TextView) rootView.findViewById(R.id.txtEmail);
         btnEdit = (Button) rootView.findViewById(R.id.btnEdit);
+        btnChangePicture = (Button) rootView.findViewById(R.id.btnChangePicture);
         btnLogout = (Button) rootView.findViewById(R.id.btnLogout);
         edtNewPassword = (EditText) rootView.findViewById(R.id.edtNewPassword);
         edtConfirmPassword = (EditText) rootView.findViewById(R.id.edtConfirmPassword);
@@ -62,28 +65,108 @@ public class ManagerProfileFragment extends Fragment {
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toggleViewState();
+                handleEditPasswordButton();
+            }
+        });
+
+        btnChangePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleChangePasswordButton();
             }
         });
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                handleRightButton();
+                handleLogout();
             }
         });
+
 
         return rootView;
     }
 
+    public void handleLogout() {
+        Server.logOutUser();
+        Toast.makeText(getActivity(), "Logout Successful", Toast.LENGTH_SHORT).show();
+        Intent loginPage = new Intent(getActivity(), StartPage.class);
+        startActivity(loginPage);
+    }
+
+    public void handleEditPasswordButton() {
+        if (viewState == 0) {
+            viewState = 1; //Change Password View
+
+            txtGivenName.setVisibility(View.INVISIBLE);
+            edtNewPassword.setVisibility(View.VISIBLE);
+            txtSurname.setVisibility(View.INVISIBLE);
+            edtConfirmPassword.setVisibility(View.VISIBLE);
+            txtEmail.setVisibility(View.INVISIBLE);
+
+            btnEdit.setText("CANCEL");
+            btnChangePicture.setText("SAVE");
+        }
+        else if (viewState == 1) {
+            viewState = 0;
+
+            edtNewPassword.setVisibility(View.INVISIBLE);
+            txtGivenName.setVisibility(View.VISIBLE);
+            edtConfirmPassword.setVisibility(View.INVISIBLE);
+            txtSurname.setVisibility(View.VISIBLE);
+            txtEmail.setVisibility(View.VISIBLE);
+
+            btnEdit.setText("EDIT PASSWORD");
+            btnChangePicture.setText("CHANGE PHOTO");
+        }
+    }
+
+    public void handleChangePasswordButton() {
+        if (viewState == 1) {
+            String newPassword = edtNewPassword.getText().toString();
+            String confirmPassword = edtConfirmPassword.getText().toString();
+            int passwordValidation = validatePassword(newPassword, confirmPassword);
+            if (passwordValidation == 2) {
+                Toast.makeText(getActivity(), "Passwords do not match", Toast.LENGTH_SHORT).show();
+            }
+            else if (passwordValidation == 1) {
+                Toast.makeText(getActivity(), "Password(s) are empty", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                viewState = 0;
+                Toast.makeText(getActivity(), "Password successfully updated", Toast.LENGTH_LONG).show();
+                Server.changePassword(newPassword);
+
+                edtNewPassword.setVisibility(View.INVISIBLE);
+                txtGivenName.setVisibility(View.VISIBLE);
+                edtConfirmPassword.setVisibility(View.INVISIBLE);
+                txtSurname.setVisibility(View.VISIBLE);
+                txtEmail.setVisibility(View.VISIBLE);
+
+                btnEdit.setText("EDIT PASSWORD");
+                btnChangePicture.setText("CHANGE PHOTO");
+            }
+
+        }
+        if (viewState == 0) {
+            Intent choosePic = new Intent();
+            choosePic.setType("image/*");
+            choosePic.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(choosePic,"Select Photo"), SELECT_PHOTO_REQUEST);
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SELECT_PHOTO_REQUEST && resultCode == RESULT_OK) {
+            if (data == null || data.getData() == null) {
+                Toast.makeText(getActivity(), "Upload error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     public void handleRightButton() {
-        if (viewState == 0) {
-            Server.logOutUser();
-            Toast.makeText(getActivity(), "Logout Successful", Toast.LENGTH_SHORT).show();
-            Intent loginPage = new Intent(getActivity(), StartPage.class);
-            startActivity(loginPage);
-        }
         if (viewState == 1) {
             String newPassword = edtNewPassword.getText().toString();
             String confirmPassword = edtConfirmPassword.getText().toString();
@@ -97,10 +180,12 @@ public class ManagerProfileFragment extends Fragment {
             else {
                 Toast.makeText(getActivity(), "Password successfully updated", Toast.LENGTH_LONG).show();
                 Server.changePassword(newPassword);
-                toggleViewState();
+//                toggleViewState();
             }
         }
     }
+
+
 
     public void toggleViewState() {
         if (viewState == 0) {
