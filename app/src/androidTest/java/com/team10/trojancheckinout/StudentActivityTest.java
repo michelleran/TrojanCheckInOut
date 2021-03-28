008a1477035cf60e37cfb72e122b47d9933d1972
michelleran <ranmiche@usc.edu>
2021-03-28 13:48:38 -0400

    Don't require user to be logged in; separate delete acct test case

--- app/src/androidTest/java/com/team10/trojancheckinout/StudentActivityTest.java
+++ app/src/androidTest/java/com/team10/trojancheckinout/StudentActivityTest.java
@@ -5,37 +5,59 @@ import android.app.Instrumentation;
 import android.content.Intent;
 import android.net.Uri;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.rule.ActivityTestRule;
 
 import org.hamcrest.Matcher;
 import org.hamcrest.core.AllOf;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.team10.trojancheckinout.TestUtils.WAIT_DATA;
 import static com.team10.trojancheckinout.TestUtils.WAIT_UI;
 import static com.team10.trojancheckinout.TestUtils.sleep;
 
-/**
- * Must be logged in as Student
- */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StudentActivityTest {
 
     @Rule
-    public ActivityTestRule<StudentActivity> activityRule =
-            new ActivityTestRule<>(StudentActivity.class);
+    public ActivityScenarioRule<LoginActivity> activityRule =
+        new ActivityScenarioRule<>(LoginActivity.class);
+
+    @Before
+    public void login() {
+        // login
+        onView(withId(R.id.etEmail)).perform(typeText("student@usc.edu"));
+        onView(withId(R.id.etPassword)).perform(typeText("12345678"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.btnLogin)).perform(click());
+        sleep(WAIT_DATA);
+    }
+
+    @After
+    public void logout() {
+        onView(withId(R.id.signOutbtn)).perform(click());
+    }
 
     @Test
     public void t1_OpenStudentHistory() {
@@ -47,7 +69,7 @@ public class StudentActivityTest {
         //check if appropriate recycler view is displayed on following page
         onView(withId(R.id.student_history_list)).check(matches(isDisplayed()));
         Intents.release();
-
+        Espresso.pressBack();
     }
 
     @Test
@@ -88,10 +110,7 @@ public class StudentActivityTest {
         onView(withText(R.string.cancel)).perform(click());
     }
 
-    /*
-    Runs this test at the end, because it deletes the account and will cause NPE
-     */
-    @Test
+    @Ignore("Execute this test individually due to its destructive nature. Log in as a student beforehand.")
     public void t5_ConfirmDeleteAccountPopUp(){
         sleep(WAIT_UI);
         onView(withId(R.id.floatingActionButton2)).perform(click());
