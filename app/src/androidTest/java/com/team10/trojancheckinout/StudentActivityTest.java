a84ae6443e6660b46dfd6656ef9bdc7cc9132daf
austinhuarng <austinhu@usc.edu>
2021-03-28 09:58:05 -0700

    test added for change profile picture

--- app/src/androidTest/java/com/team10/trojancheckinout/StudentActivityTest.java
+++ app/src/androidTest/java/com/team10/trojancheckinout/StudentActivityTest.java
@@ -1,10 +1,20 @@
 package com.team10.trojancheckinout;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.net.Uri;
+
 import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.core.AllOf;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -17,6 +27,10 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.team10.trojancheckinout.TestUtils.WAIT_UI;
 import static com.team10.trojancheckinout.TestUtils.sleep;
 
+/**
+ * Must be logged in as Student
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StudentActivityTest {
 
     @Rule
@@ -24,7 +38,7 @@ public class StudentActivityTest {
             new ActivityTestRule<>(StudentActivity.class);
 
     @Test
-    public void openStudentHistory() {
+    public void t1_OpenStudentHistory() {
         Intents.init();
         sleep(WAIT_UI);
         onView(withId(R.id.viewHistorybtn)).perform(click());
@@ -37,13 +51,26 @@ public class StudentActivityTest {
     }
 
     @Test
-    public void editProfileImage() {
+    public void t2_EditProfileImage() {
+        Matcher<Intent> expectedIntent = AllOf.allOf(IntentMatchers.hasAction(Intent.ACTION_PICK),
+                IntentMatchers.hasData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
+        Intents.init();
+        Intents.intending(expectedIntent).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, getGalleryIntent()));
+        sleep(WAIT_UI);
+        onView(withId(R.id.editImagebtn)).perform(click());
+        sleep(WAIT_UI);
+        Intents.intended(expectedIntent);
+        Intents.release();
+    }
 
-
+    private Intent getGalleryIntent() {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setData(Uri.parse("android.resource://com.team10.trojancheckinout/" + R.drawable.default_profile_picture));
+        return intent;
     }
 
     @Test
-    public void checkOut() {
+    public void t3_CheckOut() {
         sleep(WAIT_UI);
         onView(withId(R.id.checkOutbtn)).perform(click());
         sleep(WAIT_UI);
@@ -51,7 +78,7 @@ public class StudentActivityTest {
     }
 
     @Test
-    public void cancelDeleteAccountPopUp(){
+    public void t4_CancelDeleteAccountPopUp(){
         sleep(3000);
         onView(withId(R.id.floatingActionButton2)).perform(click());
         sleep(WAIT_UI);
@@ -61,8 +88,11 @@ public class StudentActivityTest {
         onView(withText(R.string.cancel)).perform(click());
     }
 
+    /*
+    Runs this test at the end, because it deletes the account and will cause NPE
+     */
     @Test
-    public void confirmDeleteAccountPopUp(){
+    public void t5_ConfirmDeleteAccountPopUp(){
         sleep(WAIT_UI);
         onView(withId(R.id.floatingActionButton2)).perform(click());
         sleep(WAIT_UI);
@@ -71,6 +101,7 @@ public class StudentActivityTest {
         //confirm delete
         onView(withText(R.string.confirm)).perform(click());
         sleep(WAIT_UI);
+        //check if returned to start page
         onView(withId(R.id.studentRegisterBtn)).check(matches(isDisplayed()));
     }
 }
