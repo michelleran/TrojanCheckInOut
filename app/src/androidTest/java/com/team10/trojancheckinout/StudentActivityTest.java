6fbef9c672f07308b601a7cb8c1d0ceb96446c02
austinhuarng <austinhu@usc.edu>
2021-03-27 23:23:33 -0700

    added tests

--- app/src/androidTest/java/com/team10/trojancheckinout/StudentActivityTest.java
+++ app/src/androidTest/java/com/team10/trojancheckinout/StudentActivityTest.java
@@ -1,7 +1,6 @@
 package com.team10.trojancheckinout;
 
 import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -9,12 +8,14 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.team10.trojancheckinout.TestUtils.WAIT_UI;
 import static com.team10.trojancheckinout.TestUtils.sleep;
-import static org.junit.Assert.*;
 
 public class StudentActivityTest {
 
@@ -34,11 +35,25 @@ public class StudentActivityTest {
 
     @Test
     public void editProfileImage() {
-    }
 
+    }
 
     @Test
     public void checkOut() {
+        sleep(WAIT_UI);
+        onView(withId(R.id.checkOutbtn)).perform(click());
+        sleep(WAIT_UI);
+        onView(withId(R.id.currentBuilding)).check(matches(withText(R.string.none)));
     }
 
+    @Test
+    public void cancelDeleteAccountPopUp(){
+        sleep(3000);
+        onView(withId(R.id.floatingActionButton2)).perform(click());
+        sleep(WAIT_UI);
+        onView(withText(R.string.delete_dialog_message)).check(matches(isDisplayed()));
+
+        //cancel delete
+        onView(withText(R.string.cancel)).perform(click());
+    }
 }
