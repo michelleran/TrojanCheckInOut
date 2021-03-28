88124c341d5540de5e4e049718c62b6141192a8d
michelleran <ranmiche@usc.edu>
2021-03-28 13:13:50 -0400

    Split into 2 tests

--- app/src/androidTest/java/com/team10/trojancheckinout/BuildingsTest.java
+++ app/src/androidTest/java/com/team10/trojancheckinout/BuildingsTest.java
@@ -10,6 +10,7 @@ import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.After;
@@ -62,9 +63,9 @@ public class BuildingsTest {
     }
 
     @Test
-    public void openBuilding_fakeCheckInThenOut() {
+    public void openBuilding_verifyDetails() {
         final String BUILDING = "Mudd Hall";
-        // select a building
+        // select building
         onView(withId(R.id.building_list))
             .perform(RecyclerViewActions.actionOnItem(
                 hasDescendant(withText(BUILDING)), click()));
@@ -96,9 +97,23 @@ public class BuildingsTest {
             onView(withId(R.id.currentBuilding)).check(matches(withText(BUILDING)));
             Espresso.pressBack();
         }
+    }
 
+    @Test
+    public void openBuilding_fakeCheckInThenOut() {
+        final String BUILDING = "Mudd Hall";
         String STUDENT = "kZx50sTS79Wp8az6NHZbJjFU6AI2";
         String NAME = "Testing, Student";
+
+        // select building
+        onView(withId(R.id.building_list))
+            .perform(RecyclerViewActions.actionOnItem(
+                hasDescendant(withText(BUILDING)), click()));
+
+        sleep(WAIT_UI);
+
+        RecyclerView list = getCurrentActivity().findViewById(R.id.building_details_students);
+        int count = list.getAdapter().getItemCount();
 
         // fake a check-in event
         Server.checkInStudent(STUDENT, "yiFJ8xf2VT4ge6aWXxCJ", new Callback<Building>() {
