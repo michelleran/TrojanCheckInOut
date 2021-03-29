27f27820cdc94533c6df6b58c203282cd3ce9d59
Rithwik Sivakumar <rithwiks@usc.edu>
2021-03-28 21:49:10 -0400

    building delete cancel validation

--- app/src/androidTest/java/com/team10/trojancheckinout/BuildingTest.java
+++ app/src/androidTest/java/com/team10/trojancheckinout/BuildingTest.java
@@ -5,18 +5,24 @@ import android.util.Log;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.Root;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.RootMatchers;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 
 import junit.framework.AssertionFailedError;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static com.team10.trojancheckinout.TestUtils.WAIT_DATA;
 import static com.team10.trojancheckinout.TestUtils.WAIT_UI;
@@ -218,7 +224,6 @@ public class BuildingTest {
         onView(withId(R.id.btnAddBuilding)).check(matches(isDisplayed()));
 
         RecyclerView list = activityRule.getActivity().findViewById(R.id.building_list);
-        RecyclerView.Adapter buildAdapt = list.getAdapter();
         int totalBuildingCount = list.getAdapter().getItemCount();
         Log.d(TAG, "verifyProperAddBuilding: " + String.valueOf(totalBuildingCount));
         onView(withId(R.id.building_list)).perform(RecyclerViewActions.scrollToPosition(totalBuildingCount - 1));
@@ -245,8 +250,26 @@ public class BuildingTest {
         onView(withId(R.id.btnBcCancel)).perform(click());
     }
 
+    @Test
+    public void testDeleteBuilding() {
 
 
+        onView(withId(R.id.tabs)).perform(selectTabAtPosition(1));
+        RecyclerView list = activityRule.getActivity().findViewById(R.id.building_list);
+        RecyclerView.Adapter buildAdapt = list.getAdapter();
+        int initialTotalBuildingCount = list.getAdapter().getItemCount();
+
+        onView(withId(R.id.building_list)).perform(RecyclerViewActions.scrollToPosition(initialTotalBuildingCount - 1));
+        onView(withRecyclerView(R.id.building_list).atPositionOnView(initialTotalBuildingCount - 1, R.id.btnBuildingEdit)).perform(click());
+
+        onView(withText("Delete Building")).inRoot(isPlatformPopup()).perform(click());
+        onView(withText("CANCEL")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(withText("CANCEL")).inRoot(isDialog()).check(matches(isDisplayed())).check(matches(isDisplayed()));
+
+        int finalTotalBuildingCount = list.getAdapter().getItemCount();
+
+        Assert.assertEquals(initialTotalBuildingCount, finalTotalBuildingCount);
+    }
 
 
     @Test
