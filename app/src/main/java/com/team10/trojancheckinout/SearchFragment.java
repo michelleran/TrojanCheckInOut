7e72142a51839dc8d191821405ed2a9f16a86038
michelleran <ranmiche@usc.edu>
2021-04-02 15:25:31 -0400

    Working on search fragments

--- app/src/main/java/com/team10/trojancheckinout/SearchFragment.java
+++ app/src/main/java/com/team10/trojancheckinout/SearchFragment.java
@@ -1,64 +1,138 @@
 package com.team10.trojancheckinout;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.team10.trojancheckinout.utils.Validator;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+
 public class SearchFragment extends Fragment {
+    private EditText start;
+    private EditText end;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private final String DATE_TIME_FORMAT = "%02d/%02d/%04d %02d:%02d PDT";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String studentName;
+    private String major;
 
-    public SearchFragment() {
-        // Required empty public constructor
-    }
+    private String buildingName;
+    private int startYear = -1;
+    private int startMonth = -1;
+    private int startDay = -1;
+    private int startHour = -1;
+    private int startMin = -1;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private int endYear = -1;
+    private int endMonth = -1;
+    private int endDay = -1;
+    private int endHour = -1;
+    private int endMin = -1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View rootView = inflater.inflate(
+            R.layout.fragment_search, container, false);
+
+        // get fields
+        start = rootView.findViewById(R.id.search_start_date);
+        start.setOnClickListener(view -> {
+            // show date picker dialog
+            final Calendar cal = Calendar.getInstance();
+            DatePickerDialog picker = new DatePickerDialog(rootView.getContext(), (datePicker, year, month, day) -> {
+                // save values
+                startYear = year;
+                startMonth = month + 1; // month is 0-indexed
+                startDay = day;
+
+                // pick time
+                TimePickerDialog timePicker = new TimePickerDialog(rootView.getContext(), (tp, hour, min) -> {
+                    // save values
+                    startHour = hour;
+                    startMin = min;
+                    // update field
+                    start.setText(String.format(Locale.US, DATE_TIME_FORMAT, startMonth, startDay, startYear, startHour, startMin));
+                }, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false);
+                timePicker.show();
+            }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+            picker.show();
+        });
+
+        end = rootView.findViewById(R.id.search_end_date);
+        end.setOnClickListener(view -> {
+            // show date picker dialog
+            final Calendar cal = Calendar.getInstance();
+            DatePickerDialog picker = new DatePickerDialog(rootView.getContext(), (datePicker, year, month, day) -> {
+                // save values
+                endYear = year;
+                endMonth = month + 1; // month is 0-indexed
+                endDay = day;
+                // pick time
+                TimePickerDialog timePicker = new TimePickerDialog(rootView.getContext(), (tp, hour, min) -> {
+                    // save values
+                    endHour = hour;
+                    endMin = min;
+                    // update field
+                    end.setText(String.format(Locale.US, DATE_TIME_FORMAT, endMonth, endDay, endYear, endHour, endMin));
+                }, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false);
+                timePicker.show();
+            }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_WEEK));
+            picker.show();
+        });
+
+        EditText nameField = rootView.findViewById(R.id.search_name);
+
+        Spinner majors = rootView.findViewById(R.id.search_major_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.majors_with_any, android.R.layout.simple_spinner_item);
+        majors.setAdapter(adapter);
+
+        Spinner buildings = rootView.findViewById(R.id.search_building_spinner);
+        // TODO: get list of buildings
+
+        Button button = rootView.findViewById(R.id.search_button);
+        button.setOnClickListener(view -> {
+            // validate that start <= end date
+            if (startYear != -1 && endYear != -1 &&
+                (startYear > endYear ||
+                    (startYear == endYear && startMonth > endMonth) ||
+                    (startYear == endYear && startMonth == endMonth && startDay > endDay) ||
+                    (startYear == endYear && startMonth == endMonth && startDay == endDay && startHour > endHour) ||
+                    (startYear == endYear && startMonth == endMonth && startDay == endDay && startHour == endHour && startMin > endMin)))
+            {
+                Toast.makeText(getContext(), R.string.filter_invalid_dates, Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            // open search results (replace this fragment)
+            final FragmentTransaction ft = getParentFragmentManager().beginTransaction();
+            ft.replace(R.id.filter_tab_content,
+                SearchResultsFragment.newInstance(
+                    nameField.getText().toString().trim(),
+                    majors.getSelectedItemPosition() == 0 ? "" : majors.getSelectedItem().toString(),
+                    buildings.getSelectedItemPosition() == 0 ? "" : buildings.getSelectedItem().toString(),
+                    startYear, startMonth, startDay, startHour, startMin,
+                    endYear, endMonth, endDay, endHour, endMin));
+            ft.commit();
+            ft.addToBackStack("search");
+        });
+
+        return rootView;
     }
 }
--- app/src/main/java/com/team10/trojancheckinout/SearchResultsFragment.java
+++ app/src/main/java/com/team10/trojancheckinout/SearchResultsFragment.java
@@ -3,10 +3,16 @@ package com.team10.trojancheckinout;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.team10.trojancheckinout.model.Callback;
+import com.team10.trojancheckinout.model.Server;
+import com.team10.trojancheckinout.model.Student;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +21,39 @@ import android.view.ViewGroup;
  */
 public class SearchResultsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_NAME = "name";
+    private static final String ARG_MAJOR = "major";
+    private static final String ARG_BUILDING = "building";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ARG_START_YEAR = "startYear";
+    private static final String ARG_START_MONTH = "startMonth";
+    private static final String ARG_START_DAY = "startDay";
+    private static final String ARG_START_HOUR = "startHour";
+    private static final String ARG_START_MIN = "startMin";
+
+    private static final String ARG_END_YEAR = "endYear";
+    private static final String ARG_END_MONTH = "endMonth";
+    private static final String ARG_END_DAY = "endDay";
+    private static final String ARG_END_HOUR = "endHour";
+    private static final String ARG_END_MIN = "endMin";
+
+    private String name;
+    private String major;
+    private String building;
+
+    private int startYear = -1;
+    private int startMonth = -1;
+    private int startDay = -1;
+    private int startHour = -1;
+    private int startMin = -1;
+
+    private int endYear = -1;
+    private int endMonth = -1;
+    private int endDay = -1;
+    private int endHour = -1;
+    private int endMin = -1;
+
+    private StudentAdapter adapter;
 
     public SearchResultsFragment() {
         // Required empty public constructor
@@ -31,17 +62,29 @@ public class SearchResultsFragment extends Fragment {
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchResultsFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static SearchResultsFragment newInstance(String param1, String param2) {
+    public static SearchResultsFragment newInstance(String name, String major, String building,
+                                                    int startYear, int startMonth, int startDay, int startHour, int startMin,
+                                                    int endYear, int endMonth, int endDay, int endHour, int endMin) {
         SearchResultsFragment fragment = new SearchResultsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
+        args.putString(ARG_NAME, name);
+        args.putString(ARG_MAJOR, major);
+        args.putString(ARG_BUILDING, building);
+
+        args.putInt(ARG_START_YEAR, startYear);
+        args.putInt(ARG_START_MONTH, startMonth);
+        args.putInt(ARG_START_DAY, startDay);
+        args.putInt(ARG_START_HOUR, startHour);
+        args.putInt(ARG_START_MIN, startMin);
+
+        args.putInt(ARG_END_YEAR, endYear);
+        args.putInt(ARG_END_MONTH, endMonth);
+        args.putInt(ARG_END_DAY, endDay);
+        args.putInt(ARG_END_HOUR, endHour);
+        args.putInt(ARG_END_MIN, endMin);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +93,21 @@ public class SearchResultsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            name = getArguments().getString(ARG_NAME);
+            major = getArguments().getString(ARG_MAJOR);
+            building = getArguments().getString(ARG_BUILDING);
+
+            startYear = getArguments().getInt(ARG_START_YEAR);
+            startMonth = getArguments().getInt(ARG_START_MONTH);
+            startDay = getArguments().getInt(ARG_START_DAY);
+            startHour = getArguments().getInt(ARG_START_HOUR);
+            startMin = getArguments().getInt(ARG_START_MIN);
+
+            endYear = getArguments().getInt(ARG_END_YEAR);
+            endMonth = getArguments().getInt(ARG_END_MONTH);
+            endDay = getArguments().getInt(ARG_END_DAY);
+            endHour = getArguments().getInt(ARG_END_HOUR);
+            endMin = getArguments().getInt(ARG_END_MIN);
         }
     }
 
@@ -59,6 +115,21 @@ public class SearchResultsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_results, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_search_results, container, false);
+
+        // set up RecyclerView
+        RecyclerView resultsList = rootView.findViewById(R.id.results_list);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        resultsList.setLayoutManager(llm);
+
+        adapter = new StudentAdapter();
+        resultsList.setAdapter(adapter);
+        Server.searchStudents(name, major, building,
+            startYear, startMonth, startDay, startHour, startMin,
+            endYear, endMonth, endDay, endHour, endMin, adapter);
+
+        return rootView;
     }
 }
