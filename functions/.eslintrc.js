40b8b0d8021eac3bad72e576bc4b3c131ee7b4fe
austinhuarng <austinhu@usc.edu>
2021-04-29 21:59:29 -0700

    new scheduled functions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   "extends": [
     "eslint:recommended",
     "google",
+    "parser:babel-eslint",
   ],
   // "parser": "babel-eslint",
   "rules": {
--- functions/index.js
+++ functions/index.js
@@ -22,94 +22,74 @@ exports.kickout = functions.https.onCall((data, context)=> {
   return admin.messaging().sendToTopic(topic, payLoad, options);
 });
 
-// eslint-disable-next-line max-len
-exports.scheduledFunctionCrontab = functions.pubsub.schedule("0 0 * * *") // 12 am every day  //uncomment this when you want it to actually work
-    // .timeZone('America/Los_Angeles') // set time zone (default is already LA)
+/* schedule syntax
+minute         0-59
+hour           0-23
+day of month   1-31
+month          1-12 (or names, see below)
+day of week    0-7 (0 or 7 is Sunday, or use names)
+*/
+
+exports.scheduledFunctionCrontab = functions.pubsub.schedule('55 21 * * *') //12 am every day  //uncomment this when you want it to actually work
+//exports.scheduledFunctionCrontab = functions.pubsub.schedule('10 19 * * *') //7:10 pm PST every day (testing purposes)
+    //.timeZone('America/Los_Angeles') // set time zone (default is already LA)
     .onRun((context) => {
-      console.log("This will be run every day at 12:00 AM Pacific!");
-      const UIDSet = new Set();
-      getStudentUsers(UIDSet);
-      return null;
+        console.log('This will be run every day at 12:00 AM Pacific!');
+        const UIDSet = new Set()
+        getStudentUsers(UIDSet);
+        return null;
+});
+
+
+
+async function getStudentUsers(UIDSet) { 
+    const UserRef = firestore.collection('users')
+    const buildingRef = firestore.collection('buildings')
+    //let bulkWriter = firestore.bulkWriter();
+    const db = admin.firestore();
+    const batch = db.batch();
+    db.Firestore
+    const payLoad = {
+        notification: {
+          title: "You have been kicked out!",
+          body: "A manager has kicked you out of your current building",
+          sound: "default",
+        },
+    };
+    
+    const options = {
+        priority: "high",
+        timeToLive: 60*60*2,
+    };
+
+    const snapshot = await UserRef.where('currentBuilding', '!=', "").get(); //all non-null or empty
+    if(snapshot.empty){
+        console.log("No matching documents");
+        return;
+    }
+    snapshot.forEach(doc => {
+        try {
+            const deleted = doc.data().deleted;
+            if (deleted == false){
+                const res = await doc.update({currentBuilding: null});
+            }
+        } 
+        catch (error) {
+            
+        }
     });
 
-
-// eslint-disable-next-line require-jsdoc
-async function getStudentUsers(UIDSet) {
-  const UserRef = firestore.collection("users");
-  const buildingRef = firestore.collection("buildings");
-  const bulkWriter = firestore.bulkWriter();
-  const payLoad = {
-    notification: {
-      title: "You have been kicked out!",
-      body: "A manager has kicked you out of your current building",
-      sound: "default",
-    },
-  };
-
-  const options = {
-    priority: "high",
-    timeToLive: 60*60*2,
-  };
-
-  UserRef.where("currentBuilding", "!=", "null")
-      .where("deleted", "==", "false").get().then((querySnapshot) => {
-        querySnapshot.forEach((documentSnapshot) => {
-          console.log(`Found document at ${documentSnapshot.ref.path}`);
-          const documentRef = firestore.doc(documentSnapshot.ref);
-          const UID = documentSnapshot.get("uid");
-
-
-          UIDSet.add(UID);
-          // try {
-          //     await db.runTransaction(async (t) => {
-          //       const doc = await t.get(documentRef);
-          //       t.update(documentRef, {"currentBuilding": "null"})
-          //     });
-          //     console.log('Transaction Success!');
-          // }
-          // catch (e) {
-          //     console.log('Transaction Failure:', e);
-          // }
-          // call austin's function using the UID
-          admin.messaging().sendToTopic(UID, payLoad, options);
-          // Server.sendToTopic(UID);
-
-
-          bulkWriter
-              // eslint-disable-next-line max-len
-              .update(documentRef, {currentBuilding: "null"}) // maybe check this
-              .then((result) => {
-                console.log("Successfully executed write at: ", result);
-              })
-              .catch((err) => {
-                console.log("Write failed with: ", err);
-              });
-        });
-      });
-
-
-  buildingRef.where("currentCapacity", "!=", 0).get().then((querySnapshot) => {
-    querySnapshot.forEach((documentSnapshot) => {
-      console.log(`Document found at path: ${documentSnapshot.ref.path}`);
-      const documentRef = firestore.doc(documentSnapshot.ref);
-      // try {
-      //     await db.runTransaction(async (t) => {
-      //         const doc = await t.get(documentRef);
-      //         t.update(documentRef, {"currentCapacity": 0})
-      // });
-      // console.log('Transaction Two Success!');
-      // }
-      // catch (e) {
-      //     console.log('Transaction Two Failure:', e);
-      // }
-      bulkWriter
-          .update(documentRef, {currentCapacity: 0}) // maybe check this
-          .then((result) => {
-            console.log("Successfully executed write at: ", result);
-          })
-          .catch((err) => {
-            console.log("Write failed with: ", err);
-          });
+    const snapshot2 = await buildingRef.where('currentCapacity', '!=', "0").get(); //all non-null and non 0
+    if(snapshot2.empty){
+        console.log("No matching documents");
+        return;
+    }
+    snapshot2.forEach(doc => {
+        try {
+            const res2 = await doc.update({currentCapacity: 0});
+        } 
+        catch (e) {
+            console.log("snapshot2", e);
+        }
     });
-  });
 }
