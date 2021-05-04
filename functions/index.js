2c16ad61a65015013eba513ac6a8ec158b53e11a
austinhuarng <austinhu@usc.edu>
2021-05-03 20:28:57 -0700

    working scheduled kickout records

--- functions/index.js
+++ functions/index.js
@@ -30,27 +30,23 @@ month          1-12 (or names, see below)
 day of week    0-7 (0 or 7 is Sunday, or use names)
 */
 
-exports.scheduledFunctionCrontab = functions.pubsub.schedule('0 17 * * *') //12 am every day  //uncomment this when you want it to actually work
-//exports.scheduledFunctionCrontab = functions.pubsub.schedule('10 19 * * *') //7:10 pm PST every day (testing purposes)
-    //.timeZone('America/Los_Angeles') // set time zone (default is already LA)
+exports.scheduledFunctionCrontab = functions.pubsub.schedule('0 0 * * *') 
     .onRun((context) => {
         console.log('This will be run every day at 12:00 AM Pacific!');
-        var checkouts = {};
+        var checkouts = [];
         let buildings = new Map();
         getStudentUsers(checkouts, buildings);
-        writeRecord(checkouts, buildings);
         return null;
 });
 
-async function getStudentUsers(checkouts) { 
+async function getStudentUsers(checkouts, buildings) { 
     const UserRef = admin.firestore().collection('users')
     const buildingRef = admin.firestore().collection('buildings')
-    //let bulkWriter = firestore.bulkWriter();
     const db = admin.firestore();
     const batch = db.batch();
     var d = new Date();
     const epochDate = Date.now();
-    var formalDate = d.toUTCString();
+    var formalDate = d.toLocaleString();
 
     const snapshot = await UserRef.where('currentBuilding', '!=', "").get(); //all non-null or empty
     if(snapshot.empty){
@@ -61,13 +57,12 @@ async function getStudentUsers(checkouts) {
         try {
             const deleted = doc.data().deleted;
             if (deleted == false){
-                var recordObject = {buildingId: "", buildingName: doc.data().currentBuilding, 
+                var recordObject = {buildingId: doc.data().currentBuilding, buildingName: "", 
                                     checkIn: false, epochTime: epochDate, major: doc.data().major, 
                                     studentId: doc.data().id, studentUid: doc.data().uid, 
                                     time: formalDate};
-                checkouts = recordObject;
+                checkouts.push(recordObject);
                 batch.update(doc.ref, {currentBuilding: null});
-                //const res = await doc.update({currentBuilding: null});
             }
         } 
         catch (error) {
@@ -75,26 +70,21 @@ async function getStudentUsers(checkouts) {
         }
     });
 
-    const snapshot2 = await buildingRef.where('currentCapacity', '!=', "0").get(); //all non-null and non 0
+    const snapshot2 = await buildingRef.where('currentCapacity', '!=', 0).get(); //all non-null and non 0
     if(snapshot2.empty){
         console.log("No matching documents");
         return;
     }
     snapshot2.forEach(doc => {
         try {
-            buildings.set(doc.data().name, doc.data().id); //get building ids
+            buildings.set(doc.data().id, doc.data().name); //get building ids
             batch.update(doc.ref, {currentCapacity: 0});
-            //const res2 = await doc.update({currentCapacity: 0});
         } 
         catch (e) {
             console.log("snapshot2", e);
         }
     });
-    await batch.commit();
-}
 
-async function writeRecord(checkouts, buildings){
-    
     const payLoad = {
         notification: {
           title: "You have been kicked out!",
@@ -108,11 +98,13 @@ async function writeRecord(checkouts, buildings){
         timeToLive: 60*60*2,
     };
     
-    const db = admin.firestore();
     for( i = 0; i<checkouts.length; i++){
-        checkouts[i].buildingId = buildings.get(checkouts[i].buildingName); //set the Id
+        checkouts[i].buildingName = buildings.get(checkouts[i].buildingId); //set the Id
         admin.messaging().sendToTopic(checkouts[i].studentUid, payLoad, options);
+        console.log("entered checkouts loop");
+        var checkouts_object = JSON.parse(JSON.stringify(checkouts[i]));
+        const res = await db.collection('records').add(checkouts_object);
     }
-    var checkouts_object = JSON.parse(JSON.stringify(checkouts));
-    const res = await db.collection('records').add(checkouts_object);
+
+    await batch.commit();
 }
