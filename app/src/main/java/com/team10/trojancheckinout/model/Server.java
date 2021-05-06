f5dfe28ad34eae75c997bc9d73a6b005bd8a4b1c
vikram-kher <vikramkher@live.com>
2021-05-05 17:44:51 -0700

    Removed transaction

--- app/src/main/java/com/team10/trojancheckinout/model/Server.java
+++ app/src/main/java/com/team10/trojancheckinout/model/Server.java
@@ -530,28 +530,21 @@ public class Server {
                     // Check if building already exists with new name
                     if(task.getResult().isEmpty()){
                         final DocumentReference sfDocRef = db.collection(BUILDING_COLLECTION).document(id);
-                        db.runTransaction(new Transaction.Function<Void>() {
-                            @Override
-                            public Void apply(Transaction transaction) throws FirebaseFirestoreException {
-                                DocumentSnapshot snapshot = transaction.get(sfDocRef);
-                                transaction.update(sfDocRef, "name", newName);
-                                // Success
-                                return null;
-                            }
-                        }).addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Log.d(TAG, "Transaction success!");
-                                callback.onSuccess(aVoid);
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Transaction failure.", e);
-                                callback.onFailure(e);
-                            }
-                        });
+                        sfDocRef.update("name", newName)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "DocumentSnapshot successfully updated!");
+                                        callback.onSuccess(aVoid);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error updating document", e);
+                                        callback.onFailure(e);
+                                    }
+                                });
                     }else{
                         callback.onFailure(new Exception("A building with the supplied name already exists"));
                     }
